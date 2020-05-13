66fe69d44167c13710cf2224ab543ceb846016a8
tlili bechir <bechir.tlili@etudiant-fst.utm.tn>
2020-05-13 23:17:43 +0100

    Intial commit, created spring boot project and configure the application properties

---
+++ fooddelivery/src/main/java/tn/mastere/fooddelivery/FoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package tn.mastere.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryApplication.class, args);
+	}
+
+}
